dd784ba311a857d35232fc421395b4e03184a800
mjsanchez@york.edu <mjsanchez@york.edu>
2024-03-24 19:10:55 -0500

    init

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,24 @@
-console.log('It worked!');
+//import express, { Request,Response } from "express";
+
+import { Request, Response } from "express"
+
+const express = require('express')
+const app = express()
+
+app.get('/', function (req: Request, res: Response) {
+  res.send('Hello World')
+})
+
+app.get('/hello', (req: Request, res:Response) => {
+  const name = req.query.name
+
+  if (name) {
+    res.send(`Hello ${name}! Welcome to the best server ever!`);
+  } else {
+    res.send(`Hello! Welcome to the best server ever!`);
+  }
+})
+
+app.listen(3000, () => {
+  console.log(`Server is running`);
+})
